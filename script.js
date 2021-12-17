4ba71eeec4b383ba6533df7ee5c891819363da7f
Albert Ho <albertcyi.ho@gmail.com>
2021-12-17 12:50:42 -0800

    refactor options object to consist of individual title, chart, bars objects

--- script.js
+++ script.js
@@ -4,40 +4,42 @@
 // dummy data
 const data = [3, 4, 5, 1, 2, 6, 7, 8];
 const options = {
-  title: "Bar Chart",
-  titleColor: "#222",
-  titleSize: "2rem",
-  mainAxis: "y", // x or y
-
-  // chart
-  xAxis: 15,
-  yAxis: 10,
-  units: "cm",
-  backgroundColor: "#ccc",
-  tickInterval: 2,
-
-  // bars
-  valuesPosition: "center", // flex-start, center, flex-end
-  spacing: "20", // 0 - 100
-  labels: ["three", "four", "five", "one", "two", "six", "seven", "eight"],
-  barColors: ["#222", "#444", "#666"],
-  valueColors: ["#f00", "#0f0", "#00f"],
-  labelColors: ["#f00", "#0f0", "#00f"],
+  title: {
+    text: "Bar Chart",
+    color: "#222",
+    fontSize: "2rem",
+  },
+  chart: {
+    mainAxis: "x", // x or y
+    xAxis: 15,
+    yAxis: 10,
+    units: "cm",
+    backgroundColor: "#ccc",
+    tickInterval: 2,
+  },
+  bars: {
+    valuesPosition: "center", // flex-start, center, flex-end
+    spacing: "20", // 0 - 100
+    labels: ["three", "four", "five", "one", "two", "six", "seven", "eight"],
+    colors: ["#222", "#444", "#666"],
+    valueColors: ["#f00", "#0f0", "#00f"],
+    labelColors: ["#f00", "#0f0", "#00f"],
+  },
 };
 const element = $("#barChart");
 
 // main function
 function drawBarChart(data, options, element) {
   const dataLength = data.length;
-
+  const { title, chart, bars } = options;
   // set title
-  $(`<h1>${options.title}</h1>`).insertBefore(element);
-  $("h1").css("color", options.titleColor);
-  $("h1").css("font-size", options.titleSize);
+  $(`<h1>${title.text}</h1>`).insertBefore(element);
+  $("h1").css("color", title.color);
+  $("h1").css("font-size", title.fontSize);
   $("h1").css("margin-bottom", "1rem");
 
   // render chart dimensions & background color
-  let { xAxis, yAxis, units, backgroundColor } = options;
+  let { xAxis, yAxis, units, backgroundColor } = chart;
   xAxis += units;
   yAxis += units;
   element.css("width", xAxis);
@@ -50,19 +52,19 @@ function drawBarChart(data, options, element) {
       `<div class='bar' id='bar${[i]}'><p class='barValue' id='barValue${[
         i,
       ]}'>${data[i]}</p><p class='barLabel' id='barLabel${[i]}'>${
-        options.labels[i]
+        bars.labels[i]
       }</p></div>`
     );
   }
 
   // set individual bar properties
   for (let i = 0; i < dataLength; i++) {
-    let barColorIndex = i % options.barColors.length;
-    let barValueIndex = i % options.valueColors.length;
-    let barLabelIndex = i % options.labelColors.length;
-    let barColor = `${options.barColors[barColorIndex]}`;
-    let valueColor = `${options.valueColors[barValueIndex]}`;
-    let labelColor = `${options.labelColors[barLabelIndex]}`;
+    let barColorIndex = i % bars.colors.length;
+    let barValueIndex = i % bars.valueColors.length;
+    let barLabelIndex = i % bars.labelColors.length;
+    let barColor = `${bars.colors[barColorIndex]}`;
+    let valueColor = `${bars.valueColors[barValueIndex]}`;
+    let labelColor = `${bars.labelColors[barLabelIndex]}`;
 
     // set bar color, value & label colors
     $(`#bar${[i]}`).css("background-color", barColor);
@@ -71,15 +73,15 @@ function drawBarChart(data, options, element) {
   }
 
   // set bar properties according to axis
-  const barValue = (1 / dataLength) * (100 - options.spacing) + "%";
+  const barValue = (1 / dataLength) * (100 - bars.spacing) + "%";
 
   // x-axis
-  if (options.mainAxis === "x") {
+  if (chart.mainAxis === "x") {
     $(".bar").css("width", barValue);
-    $(".bar").css("align-items", options.valuesPosition);
+    $(".bar").css("align-items", bars.valuesPosition);
 
     // set ticks
-    for (let i = 0; i <= options.yAxis; i += options.tickInterval) {
+    for (let i = 0; i <= chart.yAxis; i += chart.tickInterval) {
       element.append(
         `<div class='tick' style="bottom: ${i}${units}"><p class='tickLabel'>${i}</p></div>`
       );
@@ -87,17 +89,17 @@ function drawBarChart(data, options, element) {
 
     // set bar yAxis
     for (let i = 0; i < dataLength; i++) {
-      let yAxis = `${data[i]}${options.units}`;
+      let yAxis = `${data[i]}${units}`;
       $(`#bar${[i]}`).css("height", yAxis);
     }
 
     // y-axis
-  } else if (options.mainAxis === "y") {
+  } else if (chart.mainAxis === "y") {
     $("#barChart").css("flex-direction", "column");
     $("#barChart").css("align-items", "flex-start");
     $(".bar").css("height", barValue);
     $(".bar").css("align-items", "center");
-    $(".bar").css("justify-content", options.valuesPosition);
+    $(".bar").css("justify-content", bars.valuesPosition);
     $(".barLabel").css("position", "absolute");
     $(".barLabel").css("height", "fit-content");
     $(".barLabel").css("left", "-0.5rem");
@@ -105,7 +107,7 @@ function drawBarChart(data, options, element) {
     $(".barLabel").css("transform", "translate(-100%, 50%)");
 
     // set ticks
-    for (let i = 0; i <= options.xAxis; i += options.tickInterval) {
+    for (let i = 0; i <= chart.xAxis; i += chart.tickInterval) {
       element.append(
         `<div class='tick' style="left: ${i}${units}"><p class='tickLabel'>${i}</p></div>`
       );
@@ -118,7 +120,7 @@ function drawBarChart(data, options, element) {
 
     // set bar xAxis
     for (let i = 0; i < dataLength; i++) {
-      let xAxis = `${data[i]}${options.units}`;
+      let xAxis = `${data[i]}${chart.units}`;
       $(`#bar${[i]}`).css("width", xAxis);
     }
   }
