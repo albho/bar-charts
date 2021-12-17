ee6389e3c71390bf25776e97dce77dad1b69e3c2
Albert Ho <albertcyi.ho@gmail.com>
2021-12-17 09:51:40 -0800

    customize individual bar height, value & label colors

--- script.js
+++ script.js
@@ -2,29 +2,31 @@
 /* eslint-disable comma-dangle */
 
 // dummy data
-const data = [3, 4, 5, 1, 2];
+const data = [3, 4, 5, 1, 2, 6, 7, 8];
 const options = {
   title: "Bar Chart",
   titleColor: "#222",
   titleSize: "2rem",
 
   // chart
-  width: "10",
+  width: "15",
   height: "10",
   units: "cm",
   backgroundColor: "#ccc",
 
   // bars
   valuesPosition: "center", // flex-start, center, flex-end
-  labels: ["three", "four", "five", "one", "two"],
-  valuesColor: "#ccc",
-  labelsColor: "#222",
-  barsColor: "#222",
+  labels: ["three", "four", "five", "one", "two", "six", "seven", "eight"],
+  barColors: ["#222", "#444", "#666"],
+  valueColors: ["#f00", "#0f0", "#00f"],
+  labelColors: ["#f00", "#0f0", "#00f"],
 };
 const element = $("#barChart");
 
 // main function
 function drawBarChart(data, options, element) {
+  const dataLength = data.length;
+
   // set title
   $(`<h1>${options.title}</h1>`).insertBefore(element);
   $("h1").css("color", options.titleColor);
@@ -39,23 +41,37 @@ function drawBarChart(data, options, element) {
   element.css("background-color", backgroundColor);
 
   // render individual bars
-  for (let i = 0; i < data.length; i++) {
+  for (let i = 0; i < dataLength; i++) {
     element.append(
-      `<div class='bar' id='bar${[i]}' style='height: ${data[i]}${
-        options.units
-      }'><p class='barValue'>${data[i]}</p><p class='barLabel'>${
+      `<div class='bar' id='bar${[i]}'><p class='barValue' id='barValue${[
+        i,
+      ]}'>${data[i]}</p><p class='barLabel' id='barLabel${[i]}'>${
         options.labels[i]
       }</p></div>`
     );
   }
 
+  // set individual bar properties
+  for (let i = 0; i < dataLength; i++) {
+    let barColorIndex = i % options.barColors.length;
+    let barValueIndex = i % options.valueColors.length;
+    let barLabelIndex = i % options.labelColors.length;
+    let height = `${data[i]}${options.units}`;
+    let barColor = `${options.barColors[barColorIndex]}`;
+    let valueColor = `${options.valueColors[barValueIndex]}`;
+    let labelColor = `${options.labelColors[barLabelIndex]}`;
+
+    // set bar height, color, value & label colors
+    $(`#bar${[i]}`).css("height", height);
+    $(`#bar${[i]}`).css("background-color", barColor);
+    $(`#barValue${[i]}`).css("color", valueColor);
+    $(`#barLabel${[i]}`).css("color", labelColor);
+  }
+
   // set bar properties
-  const barWidth = (1 / data.length) * 80 + "%";
-  $(".bar").css("background-color", options.barsColor);
+  const barWidth = (1 / dataLength) * 80 + "%";
   $(".bar").css("width", barWidth);
   $(".bar").css("align-items", options.valuesPosition);
-  $(".barValue").css("color", options.valuesColor);
-  $(".barLabel").css("color", options.labelsColor);
 }
 
 // call main function
