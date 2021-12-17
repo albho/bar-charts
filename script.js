5ca817ed1797d283e09f39fe1fffa4a42e7aad5a
Albert Ho <albertcyi.ho@gmail.com>
2021-12-16 17:37:14 -0800

    render chart dimensions & color

--- script.js
+++ script.js
@@ -1,1 +1,25 @@
+// dummy data
+const data = [1, 2, 3, 4, 5];
+const options = {
+  width: "10cm",
+  height: "10cm",
+  backgroundColor: "lightgrey",
+};
+const element = "#barChart";
 
+// main function
+function drawBarChart(data, options, element) {
+  // render chart dimensions & background color
+  const { width, height, backgroundColor } = options;
+  $(element).css("width", width);
+  $(element).css("height", height);
+  $(element).css("background-color", backgroundColor);
+
+  // render individual bars
+  data.forEach((bar) => {
+    $(element).append(`<div class='data${bar}'>${bar}</div>`);
+  });
+}
+
+// call main function
+drawBarChart(data, options, element);
