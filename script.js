5148c0e5a6fbb268413178c1e27041fb3a93b4d5
Albert Ho <albertcyi.ho@gmail.com>
2021-12-16 18:01:38 -0800

    render bars & created helper fxn for setting chart properties

--- script.js
+++ script.js
@@ -1,24 +1,40 @@
 // dummy data
 const data = [1, 2, 3, 4, 5];
 const options = {
-  width: "10cm",
-  height: "10cm",
+  width: "10",
+  height: "10",
+  units: "cm",
   backgroundColor: "lightgrey",
+  barsColor: "#333",
 };
 const element = "#barChart";
 
 // main function
 function drawBarChart(data, options, element) {
+  // render individual bars
+  data.forEach((bar) => {
+    $(element).append(
+      `<div class='bar' id='bar${bar}' style='height: ${bar}${options.units}'>${bar}</div>`
+    );
+  });
+
+  setProperties(options, element);
+}
+
+// set chart properties
+function setProperties(options, element) {
   // render chart dimensions & background color
-  const { width, height, backgroundColor } = options;
+  let { width, height, units, backgroundColor } = options;
+  width += units;
+  height += units;
   $(element).css("width", width);
   $(element).css("height", height);
   $(element).css("background-color", backgroundColor);
 
-  // render individual bars
-  data.forEach((bar) => {
-    $(element).append(`<div class='data${bar}'>${bar}</div>`);
-  });
+  // bar properties
+  const barWidth = (1 / data.length) * 100;
+  $(".bar").css("background-color", options.barsColor);
+  $(".bar").css("width", `${barWidth}${units}`);
 }
 
 // call main function
